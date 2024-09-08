92c78b9ede3baae67ece540115a38abd7552f96d
alex <nalleballeuwu@gmail.com>
2024-09-08 15:42:18 +0200

    added second function + forgot the tax on the first function

--- src/Main.java
+++ src/Main.java
@@ -18,11 +18,14 @@ public class Main {
             try {
                 menu.managerMenu();
 
+                double accountBalance = 100000;
+
                 userInput = scanner.nextInt();
 
                 if (userInput == 1){
                     menu.EmployeeMenu();
                     userInput = scanner.nextInt();
+                    double tax = 0.70;
 
                     int[] arr = new int[userInput];
 
@@ -33,11 +36,18 @@ public class Main {
 
                     System.out.println("You have now paid: ");
                     for (int i = 0; i < userInput; i++) {
-                        System.out.print("Employee #" + (i + 1) + ": " + arr[i] + " kr\n");
+                        System.out.print("Employee #" + (i + 1) + ": Before tax: " + arr[i] + " kr " + "After tax: " + (arr[i] * tax) + " kr\n");
                     }
                 }
                 else if (userInput == 2) {
+                    menu.invoiceCreator();
+                    userInput = scanner.nextInt();
 
+                    double vat = userInput * 0.25;
+                    double afterTax = userInput - vat;
+                    double addToBalance = afterTax + accountBalance;
+
+                    System.out.println("Total sum: " + userInput + " kr \n" + "Total VAT: " + (vat) + " kr" + "\nNet amount: " + (afterTax) + " kr" + "\nTotal account balance: " + addToBalance + " kr");
                 }
 
                 else if (userInput == 3){
@@ -47,13 +57,9 @@ public class Main {
                 else if (userInput == 4){
                     break;
                 }
-
-                else {
-                    System.out.println("Invalid input. Enter 1, 2, 3 or 4.");
-                }
             }
             catch (InputMismatchException e) {
-                System.out.println("ERROR: not a number, try again.");
+                System.out.println("Not a valid input, try again.");
                 scanner.next();
             }
         }
